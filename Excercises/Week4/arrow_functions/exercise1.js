4241766e496477f89ca0d98c8e8375c3ff5190c2
aleha2001 <alehak2001@gmail.com>
2023-06-20 18:35:44 +0300

    arrow functions added

---
+++ Excercises/Week4/arrow_functions/exercise1.js
@@ -0,0 +1,14 @@
+function pushPull(callback) {
+  callback();
+}
+
+const push = function () {
+  console.log("pushing it!");
+};
+
+const pull = function () {
+  console.log("pulling it!");
+};
+
+pushPull(push); //should print "pushing it!"
+pushPull(pull); //should print "pulling it!"
---
+++ Excercises/Week4/arrow_functions/exercise4.js
@@ -0,0 +1,2 @@
+const sum3 = (num1, num2, num3) => num1 + num2 + num3;
+console.log(sum3(1, 2, 3));
